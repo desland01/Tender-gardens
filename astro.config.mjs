7884d91d47c3fa7425d6c14f3221314ff54dbe10
Desmond Landry <desmond@grovestreetpainting.com>
2025-10-24 14:02:21 -0400

    Configure Astro for Vercel serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  adapter: vercel({ mode: "serverless" }),
+  output: "server",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [
+    sitemap(),
+  ],
+});
